ab548c37378b5afdcbca28895bd68b9c34533a1c
kgusdorf <kgusdorf@gmail.com>
2021-02-24 09:20:11 -0800

    finished

--- src/component/ImagePage.js
+++ src/component/ImagePage.js
@@ -7,41 +7,35 @@ import pic5 from '../assets/images/pic5.jpg';
 import pic6 from '../assets/images/pic6.jpg';
 
 export default class ImagePage extends Component {
-  constructor(props) {
-    super(props);
-    const imgOverlay = document.createElement('div');
-    imgOverlay.id = 'imgOverlay';
-    document.body.appendChild(imgOverlay);
+  handleImgClick(e) {
+    let imgOverlay = document.getElementById('imgOverlay');
+    imgOverlay.className = 'active';
+    const img = document.createElement('img');
+    img.src = e.target.src;
+    while (imgOverlay.firstChild) {
+      imgOverlay.removeChild(imgOverlay.firstChild);
+    }
+    imgOverlay.appendChild(img);
+  }
 
-    const images = document.querySelectorAll('img');
-    images.forEach((image) => {
-      image.addEventListener('click', (e) => {
-        imgOverlay.className = 'active';
-        const img = document.createElement('img');
-        img.src = image.src;
-        while (imgOverlay.firstChild) {
-          imgOverlay.removeChild(imgOverlay.firstChild);
-        }
-        imgOverlay.appendChild(img);
-      });
-    });
-
-    imgOverlay.addEventListener('click', (e) => {
-      if (e.target === e.currentTarget) {
-        imgOverlay.classList.remove('active');
-      }
-    });
+  handleOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+      e.target.classList.remove('active');
+    }
   }
 
   render() {
     return (
-      <div className="images-grid">
-        <img src={pic1} alt="pic1" width="100%"></img>
-        <img src={pic2} alt="pic2" width="100%"></img>
-        <img src={pic3} alt="pic3" width="100%"></img>
-        <img src={pic4} alt="pic4" width="100%"></img>
-        <img src={pic5} alt="pic5" width="100%"></img>
-        <img src={pic6} alt="pic6" width="100%"></img>
+      <div>
+        <div id="imgOverlay" onClick={this.handleOverlayClick}></div>
+        <div className="images-grid">
+          <img src={pic1} alt="pic1" width="100%" onClick={this.handleImgClick}></img>
+          <img src={pic2} alt="pic2" width="100%" onClick={this.handleImgClick}></img>
+          <img src={pic3} alt="pic3" width="100%" onClick={this.handleImgClick}></img>
+          <img src={pic4} alt="pic4" width="100%" onClick={this.handleImgClick}></img>
+          <img src={pic5} alt="pic5" width="100%" onClick={this.handleImgClick}></img>
+          <img src={pic6} alt="pic6" width="100%" onClick={this.handleImgClick}></img>
+        </div>
       </div>
     );
   }
--- src/component/VideoPage.js
+++ src/component/VideoPage.js
@@ -5,13 +5,67 @@ import vid3 from '../assets/videos/vid3.mp4';
 import vid4 from '../assets/videos/vid4.mp4';
 
 export default class VideoPage extends Component {
+  handleVidClick(e) {
+    e.preventDefault();
+    let vidOverlay = document.getElementById('imgOverlay');
+    vidOverlay.className = 'active';
+    
+    const vid = document.createElement('video');
+    vid.width = e.target.width;
+    vid.height = e.target.height;
+    vid.src = e.target.src;
+    vid.controls = true;
+
+    while (vidOverlay.firstChild) {
+      vidOverlay.removeChild(vidOverlay.firstChild);
+    }
+    vidOverlay.appendChild(vid);
+  }
+
+  handleOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+      e.target.classList.remove('active');
+    }
+  }
+
   render() {
     return (
-      <div className="images-grid">
-        <video width="420" height="315" src={vid1} type="video/mp4" controls></video>
-        <video width="420" height="315" src={vid2} type="video/mp4" controls></video>
-        <video width="420" height="315" src={vid3} type="video/mp4" controls></video>
-        <video width="420" height="315" src={vid4} type="video/mp4" controls></video>
+      <div>
+        <div id="imgOverlay" onClick={this.handleOverlayClick}></div>
+        <div className="images-grid">
+          <video
+            width="420"
+            height="315"
+            src={vid1}
+            type="video/mp4"
+            controls
+            onClick={this.handleVidClick}
+          ></video>
+          <video
+            width="420"
+            height="315"
+            src={vid2}
+            type="video/mp4"
+            controls
+            onClick={this.handleVidClick}
+          ></video>
+          <video
+            width="420"
+            height="315"
+            src={vid3}
+            type="video/mp4"
+            controls
+            onClick={this.handleVidClick}
+          ></video>
+          <video
+            width="420"
+            height="315"
+            src={vid4}
+            type="video/mp4"
+            controls
+            onClick={this.handleVidClick}
+          ></video>
+        </div>
       </div>
     );
   }
